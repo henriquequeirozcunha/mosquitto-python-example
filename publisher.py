2a64e2ebabd9d9a460f2d525a2080d1214125500
henriquequeirozcunha <henrique.cunha@corpus.inf.br>
2024-08-30 19:40:27 -0300

    feat: save code

--- publisher.py
+++ publisher.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-""" 
+"""
 Publish some messages to queue
 """
 import paho.mqtt.publish as publish
@@ -16,10 +16,20 @@ host = "localhost"
 
 if __name__ == '__main__':
     # publish a single message
-    publish.single(topic="kids/yolo", payload="just do it", hostname=host)
+    while True:
+        import random as rand
+        import json
+        payload1 = rand.randint(1, 10)
+        payload2 = rand.randint(11, 20)
+        # encoded = json.dumps(, indent=2).encode('utf-8')
+        # print(type(encoded))
+        publish.single(topic="test", payload=json.dumps({'Vel. Média': payload1, 'Pot.': payload2}), hostname=host, port=3001)
+
+        # publish.single(topic="fernando", payload=payload2, hostname=host, port=3001)
+
 
     # publish multiple messages
-    publish.multiple(msgs, hostname=host)
+    # publish.multiple(msgs, hostname=host)
 
 
 # vi: set fileencoding=utf-8 :
